cef00772e9d419c95b2dd9a3ed0d1efbe6d24665
LIANG XIAOTONG <14768760555@163.com>
2022-11-24 23:08:45 +0900

    api1,2 completed, index.ejs modified

--- RUTROLL/app.js
+++ RUTROLL/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var router = require('./router/Test')(app);
+var router = require('./router/main')(app);
 var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
